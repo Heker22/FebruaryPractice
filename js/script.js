029a894adb735453d109ebab69f77fbec68d6afb
Heker22 <146105661+Heker22@users.noreply.github.com>
2026-02-09 14:01:35 +0100

    xz

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*
+const totalPrice = 1000;
+const discount = 0.1;
+const discountedPrice = totalPrice * (1 - discount);
+
+console.log(discountedPrice);
+
+
+
+const name = 'Matviy Holovatyi';
+
+console.log(name.length);*/
+
+function checkNumber(){
+    const value = Number(document.getElementById('number').value);
+    const result = document.getElementById('result');
+
+    if(value > 0){
+        result.textContent = 1; 
+    }
+    else if(value < 0){
+        result.textContent = -1;
+    }
+    else{
+        result.textContent = 0;
+    }
+}
